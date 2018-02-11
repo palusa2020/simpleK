00551120930606171627f7f87a86e1b375e3d311
palusa <palusa2020@gmail.com>
2018-02-10 21:00:25 -0500

    a

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # simpleK
+Maven clean , Put and Get Data.
+
+For version 0.10
+
+mvn clean install
+java -cp target/home_program-0.0.1-SNAPSHOT-jar-with-dependencies.jar io.home.PutData
+java -cp target/home_program-0.0.1-SNAPSHOT-jar-with-dependencies.jar io.home.GetData
+
+
+For version 1.0
+
+mvn clean install
+java -cp target/home_program-0.0.1-SNAPSHOT-jar-with-dependencies.jar io.home.PutData
+java -cp target/home_program-0.0.1-SNAPSHOT-jar-with-dependencies.jar io.home.GetData
+
+
+
---
+++ src/main/java/io/home/Person.java
@@ -0,0 +1,21 @@
+package io.home;
+import java.util.*;
+import org.apache.kafka.clients.producer.*;
+
+public class Person {
+public static void main ( String [] args ){
+ Properties props = new Properties();
+ props.put("bootstrap.servers", "localhost:9092");
+ props.put("acks", "all");
+ props.put("retries", 0);
+ props.put("batch.size", 16384);
+ props.put("linger.ms", 1);
+ props.put("buffer.memory", 33554432);
+ props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+ props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+ Producer<String, String> producer = new KafkaProducer<>(props);
+     producer.send(new ProducerRecord<String, String>("my-topic", "PalUSA", "CA"));
+ producer.close();
+}
+}
